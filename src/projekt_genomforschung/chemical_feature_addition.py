ce5c8e265e21f3f01c30028738c6ed4267f6cbe7
Juliane Roßmanith <jrossmanith@techfak.uni-bielefeld.de>
2026-07-24 14:54:23 +0200

    Start for generalized data download and data harmonization

---
+++ src/projekt_genomforschung/chemical_feature_addition.py
@@ -0,0 +1,91 @@
+from rdkit import Chem
+from rdkit.Chem import AllChem
+from rdkit.Chem import ChemicalFeatures
+from rdkit import RDConfig
+import os
+import pandas as pd
+import numpy as np
+import pubchempy as pcp
+
+def get_mfp(smiles):
+    """
+    Takes a SMILES string and returns a numpy array of the Morgan fingerprint (1024 bits).
+    """
+    try:
+        morganfp = AllChem.GetMorganFingerprintAsBitVect(Chem.MolFromSmiles(smiles),
+                                                         radius=2, nBits=1024)
+        return np.array(morganfp)
+    except:
+        return None
+
+def get_pharmacophore_features(smiles):
+    """
+    Takes a SMILES string and returns a dictionary containing the number of chemical features found (e.g., {‘Donor’: 2, ‘Acceptor’: 4}).
+    """
+    # initialise the "feature factory"
+    # RDKIT provides a standard file (BaseFeatures.fdef) that defines what exactly a H-donor, acceptor, etc. is.
+    fdef_name = os.path.join(RDConfig.RDDataDir, 'BaseFeatures.fdef')
+    factory = ChemicalFeatures.BuildFeatureFactory(fdef_name) # is a MolChemicalFeatureFactory object
+    if pd.isna(smiles):
+        return {}
+    
+    mol = Chem.MolFromSmiles(smiles)
+    if mol is None:
+        return {}
+        
+    # extract all features for the molecule
+    features = factory.GetFeaturesForMol(mol)
+    
+    # count how many times each feature family appears
+    feature_counts = {}
+    for feat in features:
+        family = feat.GetFamily()
+        feature_counts[family] = feature_counts.get(family, 0) + 1
+            
+    return feature_counts
+
+def smiles_to_morgan_columns(df, smiles_col='SMILES', n_bits=1024, radius=2):
+    # Calculates 1024-bit Morgan Fingerprints and expands them into individual columns.
+    mols = [Chem.MolFromSmiles(s) if pd.notna(s) else None for s in df[smiles_col]]
+
+    fingerprints = []
+    for mol in mols:
+        if mol:
+            # Generate the bit vector
+            bv = AllChem.GetMorganFingerprintAsBitVect(mol, radius=radius, nBits=n_bits)
+            # Convert to list of 0s and 1s
+            fingerprints.append(list(bv))
+        else:
+            # Maintain row alignment with a list of zeros if SMILES was invalid
+            fingerprints.append([0] * n_bits)
+
+    # Create new DataFrame from the list of lists
+    fp_df = pd.DataFrame(fingerprints, index=df.index)
+
+    # Rename columns to 'Bit_0', 'Bit_1', etc.
+    fp_df.columns = [f'Bit_{i}' for i in range(n_bits)]
+    fp_df = fp_df.astype('uint8')
+
+    # Concatenate with the original DataFrame
+    result_df = pd.concat([df, fp_df], axis=1)
+    print("Fingerprints expanded into columns.")
+    return result_df
+
+
+# Define the function to fetch SMILES via PubChem CID
+def fetch_smiles_by_cid(drug_id):
+    try:
+        # Clean the ID (ensure it is an integer)
+        clean_cid = int(drug_id)
+        
+        # Add a small delay to respect PubChem API rate limits (5 requests/sec)
+        time.sleep(0.2) 
+        
+        # Fetch the compound and return the canonical SMILES
+        compound = pcp.get_compounds(clean_cid, namespace='cid')[0]
+        return compound.connectivity_smiles
+        
+    except Exception as e:
+        # Print the exact ID that failed for easier debugging later
+        print(f"Failed to fetch SMILES for CID {drug_id}. Reason: {e}")
+        return None
---
+++ src/projekt_genomforschung/data_download.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import time
+import re
+from pathlib import Path
+from urllib.request import urlretrieve
+import pandas as pd
+
+def download_dataset():
+    # create data directory if necessary
+    data_dir = Path.cwd() / "data"
+    data_dir.mkdir(exist_ok=True)
+
+    # get drug sensitivity dataset from GDSC2 if necessary
+    url = "https://cmp.cog.sanger.ac.uk/download/GDSC2_fitted_dose_response_27Oct23.xlsx"
+    file_path = data_dir / "GDSC2_fitted_dose_response_27Oct23.xlsx"
+    if file_path.exists():
+        print(f"{file_path.name} already exists. Skipping download.")
+    else:
+        print(f"Downloading {file_path.name}...")
+        urlretrieve(url, file_path)
+        print("Download complete.")
+
+    # get expression profiles from DepMap if necessary
+    url = "https://depmap.org/portal/data_page/?tab=allData&releasename=DepMap%20Public%2025Q3&filename=OmicsExpressionTPMLogp1HumanProteinCodingGenes.csv"
+    file_path = data_dir / "OmicsExpressionTPMLogp1HumanProteinCodingGenes.csv"
+    if file_path.exists():
+        print(f"{file_path.name} already exists. Skipping download.")
+    else:
+        print(f"Downloading {file_path.name}...")
+        urlretrieve(url, file_path)
+        print("Download complete.")
+
+    # get meta data from expression data
+    url = "https://depmap.org/portal/data_page/?tab=allData&releasename=DepMap%20Public%2025Q3&filename=Model.csv"
+    file_path = data_dir / "Model.csv"
+    if file_path.exists():
+        print(f"{file_path.name} already exists. Skipping download.")
+    else:
+        print(f"Downloading {file_path.name}...")
+        urlretrieve(url, file_path)
+        print("Download complete.")
+
+
+    # get L1000 landmark genes
+    url = "https://github.com/s-juross/projekt_genomforschung/blob/main/data/L1000.txt"
+    file_path = data_dir / "L1000.txt"
+    if file_path.exists():
+        print(f"{file_path.name} already exists. Skipping download.")
+    else:
+        print(f"Downloading {file_path.name}...")
+        urlretrieve(url, file_path)
+        print("Download complete.")
